088d4e276eeffabfdd9a213387c8e840f2e9d005
edgarsilva <edgar.osc@gmail.com>
2015-05-15 17:58:23 -0500

    Fix issue caused by having data streaming packets comming without a dataStreaming obj with appropiate masks.

--- examples/data-streaming.js
+++ examples/data-streaming.js
@@ -6,7 +6,7 @@ var orb = sphero("/dev/rfcomm0");
 orb.connect(function() {
   // options for streaming data
   var opts = {
-    n: 400,
+    n: 200,
     m: 1,
     mask1: 0x00000000,
     pcnt: 0,
@@ -20,8 +20,5 @@ orb.connect(function() {
     console.log("  data:", data);
   });
 
-  setInterval(function() {
-    var direction = Math.floor(Math.random() * 360);
-    orb.roll(150, direction);
-  }, 1000);
+  orb.roll(150, 0);
 });
