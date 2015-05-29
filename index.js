06388f609c16d5c4892637612015949f8c26c40c
Andrew Stewart <andrew@stwrt.ca>
2015-05-29 08:27:04 -0700

    Small JSDoc fixups, doc factory

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 var Sphero = require("./lib/sphero");
 
-module.exports = function sphero(address, options) {
-  return new Sphero(address, options);
+/**
+ * Creates a new Sphero instance with the provided options.
+ *
+ * @param {String} address port/UUID/address of the connected Sphero
+ * @param {Object} [opts] options for sphero setup
+ * @param {Object} [opts.adaptor] if provided, a custom adaptor used for Sphero
+ * communication
+ * @param {Number} [opts.sop2=0xFD] SOP2 value to be passed to commands
+ * @param {Number} [opts.timeout=500] delay before a command is considered dead
+ * @example var orb = sphero("/dev/rfcomm0");
+ * @example var orb = sphero("/dev/tty.Sphero-OGB-AMP-SPP", { timeout: 300 });
+ * @returns {Sphero} a new instance of Sphero
+ */
+module.exports = function sphero(address, opts) {
+  return new Sphero(address, opts);
 };
