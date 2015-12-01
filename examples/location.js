2cd953f7fb793bc491bc50130ca6d6a9bdcefba1
deadprogram <ron@hybridgroup.com>
2015-11-30 19:57:59 -0800

    Correct command ID for readLocator and properly handle 0x00 did sync command responses

---
+++ examples/location.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var sphero = require("../");
+var orb = sphero("/dev/rfcomm0");
+
+orb.connect(function() {
+  var opts = {
+    flags: 0x01,
+    x: 0x0000,
+    y: 0x0000,
+    yawTare: 0x0
+  };
+
+  orb.configureLocator(opts);
+
+  setInterval(function() {
+    orb.readLocator(function(err, data) {
+      if (err) {
+        console.log("error: ", err);
+      } else {
+        console.log("readLocator:");
+        console.log("  xpos:", data.xpos);
+        console.log("  ypos:", data.ypos);
+        console.log("  xvel:", data.xvel);
+        console.log("  yvel:", data.yvel);
+        console.log("  sog:", data.sog);
+      }
+    });
+  }, 1000);
+});
