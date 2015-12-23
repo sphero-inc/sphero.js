303876a8c4d570b3881a7d8fb006ae624041e5ed
deadprogram <ron@hybridgroup.com>
2015-12-23 07:34:00 -0800

    Cleanup logging and begin implmenting better test coverage for BLE interface

--- spec/lib/adaptors/ble.spec.js
+++ spec/lib/adaptors/ble.spec.js
@@ -1,8 +1,8 @@
 "use strict";
 
-//var serialport = require("serialport");
+var ble = require("noble");
 
-//var EventEmitter = require("events").EventEmitter;
+var EventEmitter = require("events").EventEmitter;
 
 var Adaptor = lib("adaptors/ble");
 
@@ -11,11 +11,13 @@ describe("BLE Adaptor", function() {
 
   beforeEach(function() {
     adaptor = new Adaptor("cc360e85785e");
-    //stub(serialport, "SerialPort").returns(port);
+    stub(ble, "startScanning");
+    stub(ble, "stopScanning");
   });
 
   afterEach(function() {
-    //serialport.SerialPort.restore();
+    ble.startScanning.restore();
+    ble.stopScanning.restore();
   });
 
   it("is a class", function() {
@@ -28,102 +30,64 @@ describe("BLE Adaptor", function() {
     });
   });
 
-  // describe("#open", function() {
-  //   var callback;
-  //
-  //   beforeEach(function() {
-  //     callback = spy();
-  //     adaptor.open(callback);
-  //   });
-  //
-  //   it("creates a new SerialPort instance", function() {
-  //     expect(adaptor.serialport).to.be.eql(port);
-  //   });
-  //
-  //   it("doesn't automatically trigger the callback", function() {
-  //     expect(callback).to.not.be.called;
-  //   });
-  //
-  //   context("when the serialport emits 'open'", function() {
-  //     var emit;
-  //
-  //     beforeEach(function() {
-  //       emit = adaptor.emit = spy();
-  //     });
-  //
-  //     context("if there wasn't an error", function() {
-  //       beforeEach(function() {
-  //         port.emit("open");
-  //       });
-  //
-  //       it("triggers the callback", function() {
-  //         expect(callback).to.be.called;
-  //       });
-  //
-  //       it("emits 'open'", function() {
-  //         expect(emit).to.be.calledWith("open");
-  //       });
-  //
-  //       it("passes through 'error' events", function() {
-  //         port.emit("error", "error message");
-  //         expect(emit).to.be.calledWith("error", "error message");
-  //       });
-  //
-  //       it("passes through 'close' events", function() {
-  //         port.emit("close", "port closed");
-  //         expect(emit).to.be.calledWith("close", "port closed");
-  //       });
-  //
-  //       it("passes through 'data' events", function() {
-  //         port.emit("data", "new data");
-  //         expect(emit).to.be.calledWith("data", "new data");
-  //       });
-  //     });
-  //
-  //     context("if there was an error while opening", function() {
-  //       beforeEach(function() {
-  //         port.emit("open", "error!");
-  //       });
-  //
-  //       it("triggers the callback with the error", function() {
-  //         expect(callback).to.be.calledWith("error!");
-  //       });
-  //
-  //       it("doesn't emit 'open'", function() {
-  //         expect(emit).to.not.be.calledWith("open");
-  //       });
-  //     });
-  //   });
-  // });
-
-  describe("i/o", function() {
-    //var data, callback;
+  describe("#open", function() {
+    var callback;
 
     beforeEach(function() {
-      // port.write = stub();
-      // port.close = stub();
-      //
-      // stub(adaptor, "on");
-      //
-      // data = [0x0f, 0x10, 0x03];
-      // callback = spy();
-      //
-      // adaptor.serialport = port;
+      callback = spy();
+      adaptor.open(callback);
     });
 
-    // it("#write calls @serialport#write with", function() {
-    //   adaptor.write(data, callback);
-    //   expect(adaptor.serialport.write).to.be.calledWith(data, callback);
-    // });
-    //
-    // it("#onRead calls #on with", function() {
-    //   adaptor.onRead(callback);
-    //   expect(adaptor.on).to.be.calledWith("data", callback);
-    // });
-    //
-    // it("#onRead calls #on with", function() {
-    //   adaptor.close(callback);
-    //   expect(adaptor.serialport.close).to.be.calledWith(callback);
-    // });
+    it("doesn't automatically trigger the callback", function() {
+      expect(callback).to.not.be.called;
+    });
+
+    context("when the ble interface emits 'discover'", function() {
+      var emit;
+      var desiredPeripheral = {
+        id: "cc360e85785e",
+        connect: spy()
+      };
+      var otherPeripheral = {
+        id: "ff360e85785e",
+        connect: spy()
+      };
+
+      beforeEach(function() {
+        emit = adaptor.emit = spy();
+      });
+
+      context("if the peripheral was found", function() {
+        beforeEach(function() {
+          ble.emit("discover", desiredPeripheral);
+        });
+
+        it("triggers stopScanning", function() {
+          expect(ble.stopScanning).to.be.called;
+        });
+
+        it("gets connected", function() {
+          expect(desiredPeripheral.connect).to.be.called;
+        });
+      });
+
+      context("if the peripheral was not found", function() {
+        beforeEach(function() {
+          ble.emit("discover", otherPeripheral);
+        });
+
+        it("does not trigger stopScanning", function() {
+          expect(ble.stopScanning).to.not.be.called;
+        });
+
+        it("does not get connected", function() {
+          expect(otherPeripheral.connect).to.not.be.called;
+        });
+      });
+    });
+  });
+
+  describe("i/o", function() {
+    it("#write calls @ble#writeServiceCharacteristic");
   });
 });
