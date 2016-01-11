384bd9e564e550fb76073a35e69d06cc95d7a514
Olivier Cartier <info@oliviercartier.com>
2016-01-10 23:39:56 -0500

    fix typo

--- examples/color.js
+++ examples/color.js
@@ -5,23 +5,25 @@ var orb = sphero(process.env.PORT);
 
 orb.connect(function() {
   // sets color to the provided r/g/b values
-  orb.color({ red: 255, green: 0, blue: 255 });
+  orb.color({ red: 255, green: 0, blue: 0 });
 
   setTimeout(function() {
     console.log("color 1");
     // sets color to the provided hex value
-    orb.color(0xff0000);
+    orb.color(0x0000ff);
   }, 1000);
 
   setTimeout(function() {
     console.log("color 2");
     // hex numbers can also be passed in strings
-    orb.color("00ff00");
+    orb.color("000000");
   }, 2000);
 
   setTimeout(function() {
-    console.log("color 3");
     // sets color to the provided color name
-    orb.color("magenta");
+    orb.sleep(0, 0, 0);
+    orb.disconnect(function() { console.log("bye!");});
+    process.exit();
   }, 3000);
+
 });
